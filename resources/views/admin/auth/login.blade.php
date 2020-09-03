1dc39fd17ce793b21d7bdc47b389c8bca9eaf35e
SYW <siayongwei@hotmail.com>
2020-09-03 17:12:56 +0800

    template up

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,80 +1,84 @@
-@extends('admin.layouts.app')
+<!doctype html>
+<html lang="en">
 
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
+    <head>
+        <meta charset="utf-8" />
+        <title>{{__('auth.login')}} {{config('systemname.name')}}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="{{ asset('admin-asset/images/favicon.ico') }}">
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('admin.login') }}">
-                            @csrf
+        <!-- Bootstrap Css -->
+        <link href="{{ asset('admin-asset/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="{{ asset('admin-asset/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="{{ asset('admin-asset/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+    </head>
 
-                            <div class="form-group row">
-                                <label for="email"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email"
-                                           class="form-control @error('email') is-invalid @enderror" name="email"
-                                           value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="password"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password"
-                                           class="form-control @error('password') is-invalid @enderror" name="password"
-                                           required autocomplete="current-password">
-
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember"
-                                               id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
+    <body>
+       
+        <div class="account-pages my-5 pt-sm-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6 col-xl-5">
+                        <div class="card overflow-hidden">
+                            <div class="bg-soft-primary">
+                                <div class="row">
+                                    <div class="col-7">
+                                        <div class="text-primary p-4">
+                                            <h5 class="text-primary">{{config('systemname.name')}}</h5>
+                                            <p>@lang('auth.loginmessage')</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-5 align-self-end">
+                                        <img src="{{ asset('admin-asset/images/profile-img.png') }}" alt="" class="img-fluid">
                                     </div>
                                 </div>
                             </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Login') }}
-                                    </button>
-
-                                    @if (Route::has('admin.password.request'))
-                                        <a class="btn btn-link" href="{{ route('admin.password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
-                                        </a>
-                                    @endif
+                            <div class="card-body pt-0"> 
+                                <div class="p-2">
+                                    <form class="form-horizontal" action="{{ route('admin.login') }}">
+        
+                                        <div class="form-group">
+                                            <label for="username">{{__('auth.username')}}</label>
+                                            <input type="text" class="form-control" name="username" id="username" >
+                                        </div>
+                
+                                        <div class="form-group">
+                                            <label for="userpassword">{{__('auth.password')}}</label>
+                                            <input type="password" class="form-control" name="password" id="userpassword" >
+                                        </div>
+                
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" name="remember" id="customControlInline">
+                                            <label class="custom-control-label" for="customControlInline">@lang('auth.rememberme')</label> 
+                                        </div> 
+                                        
+                                        <div class="mt-3">
+                                            <button class="btn btn-primary btn-block waves-effect waves-light" type="submit">{{__('auth.login')}}</button>
+                                        </div>
+            
+                                    </form>
                                 </div>
+            
                             </div>
-                        </form>
+                        </div>
+
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-@endsection
+
+        <!-- JAVASCRIPT -->
+        <script src="{{ asset('admin-asset/libs/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('admin-asset/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('admin-asset/libs/metismenu/metisMenu.min.js') }}"></script>
+        <script src="{{ asset('admin-asset/libs/simplebar/simplebar.min.js') }}"></script>
+        <script src="{{ asset('admin-asset/libs/node-waves/waves.min.js') }}"></script>
+        
+        <!-- App js -->
+        <script src="{{ asset('admin-asset/js/app.js') }}"></script>
+    </body>
+</html>
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
     <div class="content">
         <div class="title m-b-md">
-            Laravel
+            {{__('message.laravel')}}
         </div>
 
         <div class="links">
